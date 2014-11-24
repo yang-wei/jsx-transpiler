049a5086110fdfea10eb88d9954e5ff50cac04f9
yang-wei <yangweilim21@gmail.com>
2014-11-24 22:22:25 +0900

    bind output to input

--- app.js
+++ app.js
@@ -3,11 +3,36 @@ var render = function(component) {
 };
 
 var APP = React.createClass({
+	getInitialState: function() {
+		return {
+			input: '',
+			output: '',
+			error: ''
+		}
+	},
+	sync: function(e) {
+		var code = e.target.value;
+		try {
+			this.setState({
+				output: code,
+				error: ''
+			});
+		} catch(error) {
+			this.setState({
+				error: error.message
+			});
+		}
+	},
 	render: function() {
 		return (
-			<div className="title">
-				Hello !!!
-			</div>
+			<div className="pure-g">
+				<div className="pure-u-1-2">
+					<textarea defaultValue={this.state.input} onChange={this.sync}></textarea>
+				</div>
+				<div className="pure-u-1-2">
+					<pre>{this.state.output}</pre>
+				</div>
+			</div>	
 		);
 	}
 });
